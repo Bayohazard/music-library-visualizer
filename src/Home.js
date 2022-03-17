9d1bd4e300a5a9dc3dfe114a2a3ba00397b936be
Bryan Paez <bryanpaez2014@yahoo.com>
2022-03-16 19:46:11 -0700

    Add content to main section of homepage

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
+import vinylImage from './images/vinyl.svg';
 
 function Home() {
   return (
     <div>
       <Header />
+      <main>
+        <div className="Home flex justify-between h-full">
+          <div className="flex-row ml-48 mt-24">
+            <h1 className="text-white">Music Library Helper</h1>
+            <p className="subheading mt-24 text-white">
+              Search through and analyze your music library with ease
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
