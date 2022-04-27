9a35c13f4f458914736640f2a5cf169b2786c13d
Bryan Paez <bryanpaez2014@yahoo.com>
2022-04-26 23:16:12 -0700

    Add sliding effect to hamburger menu

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -7,26 +7,27 @@ function HamburgerMenu(props) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
-      <div
-        className="flex lg:hidden ml-12"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        {isMenuOpen ? <img src={CloseIcon} /> : <img src={HamburgerIcon} />}
+    <div className="lg:hidden">
+      <input
+        type="checkbox"
+        id="navbar-toggle"
+        className="hidden"
+        onChange={(e) => {
+          setIsMenuOpen(!isMenuOpen);
+        }}
+      />
+      <div className="nav-sidebar w-3/4 sm:w-1/3 h-full text-center">
+        <NavigationLinks
+          isMobileSidebar={true}
+          className="flex justify-center px-8"
+        />
       </div>
-      <div className="lg:hidden">
-        {isMenuOpen ? (
-          <div className="nav-sidebar w-3/4 sm:w-1/3 h-full text-center">
-            <NavigationLinks
-              isMobileSidebar={true}
-              className="flex justify-center px-8"
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
+      <label for="navbar-toggle">
+        <div className="ml-12 hover:cursor-pointer">
+          {isMenuOpen ? <img src={CloseIcon} /> : <img src={HamburgerIcon} />}
+        </div>
+      </label>
+    </div>
   );
 }
 
